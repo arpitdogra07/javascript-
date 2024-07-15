2c4326353e2db85b1999652451354d5dafa14bb1
arpitdogra07 <48236094+arpitdogra07@users.noreply.github.com>
2024-07-15 19:46:06 +0000

    dataiscommit

---
+++ basics/DandEcma.js
@@ -0,0 +1,6 @@
+"use strict" // it will treat all js code as newer version
+
+//alert(3+3) // we use node js not browser 
+
+ console.log(typeof null)
+ console.log(typeof undefined);
---
+++ basics/variables.js
@@ -0,0 +1,23 @@
+const accountId = 12345   //values cant be change
+let accountEmail ="dogra.arpit07@gmsil.com"  
+var accPass ="124"
+accCity="Jaipur" 
+
+//accountId=3
+
+//console.log(accountId);  this things not allowed
+console.log(accountId)
+
+
+accountEmail = "bha@gmail.com"
+accPass ="356"
+accCity ="Delhi"
+
+console.log([accountEmail,accPass,accCity]);
+console.table([accountEmail,accPass,accCity])
+
+
+/*
+Note -  prefer not to use var bcz of issue in block scope and functional scope
+
+*/
