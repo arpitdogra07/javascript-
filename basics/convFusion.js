29520d83748f565426c52d15ef9e3dd8c1c3b943
arpitdogra07 <48236094+arpitdogra07@users.noreply.github.com>
2024-07-16 09:28:21 +0000

    conversion of datatypes

---
+++ basics/convFusion.js
@@ -0,0 +1,131 @@
+/*
+let score = "34abc"
+console.log(typeof score)
+console.log(typeof(score))   //print-> both will print the ztring value 
+
+let value=Number(score)
+console.log(typeof value)
+console.log(value) 
+/*          // it will print nan if we can write.
+
+// if we can write it in "" .eg score ="34" it will print string.
+
+// if i write score = "34abc" it means and write and then do things liks
+// let variable = Number(score) 
+//console.log(varib) . It will print mainly nan(not a number)
+//console.log(typeof(varib)). It will print a ' number ';
+
+/*
+
+let score = "34abc"
+console.log(typeof score)
+console.log(typeof(score))   // both of the  print will same value we can write it in any type
+
+let value=Number(score)
+console.log(typeof value)
+console.log(value)           // it will print nan if we can write.
+*/
+
+
+/*
+let score = null
+console.log(typeof score)    // print -> object
+console.log(typeof(score)) // print ->object
+
+let value=Number(score)
+console.log(typeof value)      // print-> number
+console.log(value)             // print-> 0
+*/
+
+/*
+let score = "null"
+console.log(typeof score)      // print-> string
+console.log(typeof(score))     // print -> string
+
+let value=Number(score)
+console.log(typeof value)     // print -> number
+console.log(value)           //  print -> NAN (not a number )
+
+*/
+
+/*
+
+let score = undefined
+console.log(typeof score)   // print ->  undefined
+console.log(typeof(score))   //  print -> undefined
+
+let value=Number(score)
+console.log(typeof value)   // print->  undefinend
+console.log(value)           // print-> NAN (not a number)
+
+*/
+
+/*
+let score = true
+console.log(typeof score)   // print-> bool 
+console.log(typeof(score))    // print->  bool 
+
+let value=Number(score)
+console.log(typeof value)   // print-> number 
+console.log(value)          // print->  1
+*/
+
+/*
+let score = "Arpit"
+console.log(typeof score)    // print- >string by both of them 
+console.log(typeof(score))   // both of the  print will same value we can write it in any type
+                           
+
+let value=Number(score)
+console.log(typeof value)     // print -> number 
+console.log(value)           // it will print nan if we can write.
+*/
+
+/*
+
+ special note -> "33" -> conversion into number -> 33
+                   "33abc"-> it will print  nan
+                   true -> 1 and if it is false it will print 0 ;
+
+*/
+
+/*
+    let isLoggedIn = ""
+    let boolLog = Boolean(isLoggedIn)
+    console.log(boolLog)               // print -> it will print maninly false value
+
+*/
+/*
+
+    let isLoggedIn = "Arpit"
+    let boolLog = Boolean(isLoggedIn)
+    console.log(boolLog)             // print -> it will print maninly true value
+*/
+
+   /*
+     note ->   1->true
+      0-> false
+     ""->false
+     "Arpit" ->true
+     */
+  
+     /*
+     let somenumber =33
+     let stringNumber =String(somenumber)
+     console.log(stringNumber)            // print -> 33 
+     console.log(typeof(stringNumber))   // print -> string value
+
+    */
+
+     
+
+
+
+
+
+
+
+
+ 
+
+
