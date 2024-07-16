6ed25d495c7270ad6229f583695f44795ab1e1ef
arpitdogra07 <48236094+arpitdogra07@users.noreply.github.com>
2024-07-16 11:11:04 +0000

    operations data

---
+++ basics/operations.js
@@ -0,0 +1,29 @@
+  "use strict"
+/*
+  let value =3
+  let negValue=-value
+  console.log(negValue)
+*/
+
+/*
+let str1 = "Hello"
+let str2 = " Arpit"
+let str3 = str1 + str2
+console.log(str3)   // print -> Hello Arpit
+
+// to primitive datatypes
+
+console.log("1" +2)     //print ->12 
+console.log("1" + "2 "+ 3)  // print-> 12 3
+console.log(1 + 2)  //print -> 3
+
+ Note -> They will be mainly called as an primitive data SVGUnitTypes.
+ console.log(1+2+ "3")  // print 33
+*/
+
+
+
+
+
+
+
