2f363400bb0002700bd991ea76b2642854e87a59
arpitdogra07 <48236094+arpitdogra07@users.noreply.github.com>
2024-07-23 06:52:56 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,17 @@
+const name ="Arpit "
+const repo = 50
+//console.log(`My name is ${name} and my repo is ${repo}`)  // here we use backtics and string manupulation will be their
+
+
+const gamename = new String('Arpit')
+//console.log(gamename[0]);
+//console.log(gamename[1]);
+
+console.log(gamename.__proto__)  // it will give mainly the object
+console.log(gamename.toUpperCase())  // it will uppercase the string letters
+console.log(gamename.toLowerCase())// it will lowercase the letters
+console.log(gamename.charAt(3)) // In this index which character is available
+
+
+
+
